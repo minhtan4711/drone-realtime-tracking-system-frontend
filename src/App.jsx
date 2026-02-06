3ec2ee2f4340ba86781f31064974d02d10532f63
minhtan <32776120+minhtan4711@users.noreply.github.com>
2026-02-06 20:49:16 +0700

    feature: initialize project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import MapView from "./components/MapView"
+
+export default function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <MapView />
+    </div>
+  )
+}
